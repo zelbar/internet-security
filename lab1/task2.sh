ce23891a4c18f45b889cdaf287e33d9eb845abe5
zeljko-b <zeljko.baranek@fer.hr>
2017-10-14 16:45:45 +0200

    Lab1: Task 1 and Task 2

---
+++ lab1/task2.sh
@@ -0,0 +1,8 @@
+# Determining the file type
+file decrypted-data.bin
+# Result: decrypted-data.bin: ISO Media, MP4 v2 [ISO 14496-14]
+
+# Rename the file with appropriate extensions
+mv decrypted-data.bin video.mp4
+
+# The file is a video from TED talks
